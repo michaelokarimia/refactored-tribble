a34f6e4d7325b7a02e5d947e7a859136f8e33f95
Michael Okarimia <git@michaelokarimia.com>
2017-06-12 01:01:33 +0100

    Counts swaps, test all sample cases prints results
    Checks each char in both words one by one, swaps them if they dont't
    match, from start to finish.
    
    Prints out what letters are being compared, with expected and actual
    results

--- anagrams.py
+++ anagrams.py
@@ -13,30 +13,54 @@ def compareWords(firstWord, secondWord):
 
     sortedSecondWord.sort()
 
+    #quick way to check if they have the same letters, if not, its impossible for them to be anagrams
     if sortedFirstWord != sortedSecondWord:
-        '''Not an anagram'''
         return 0
     else:
-        swapsCount = countSwaps(firstWord, secondWord)
+        swapsCount = countSwaps(list(firstWord), list(secondWord))
 
-        return swapsCount * 5
+        score = swapsCount * 5
 
-def countSwaps(firstWord, secondWord):
+        if score > 100:
+            return 0
+        else:
+            return (100 - score)
 
-    swaps = 0
+def countSwaps(firstword, secondword):
 
-    for x in xrange(len(secondWord)):
-        if firstWord[x] != secondWord[x]:
-            swaps =+ 1
+    swapcount = 0
 
-    return swaps
+    wordlength = len(secondword)
+    j, i = 0,0
+    #inspect each letter of both words in order, and check if the match one another
+    while (wordlength > i):
+        j = i
+        while (firstword[j] != secondword[i]):
+            j = j+1
+            #they don't match, at least one swap is required
+            swapcount = swapcount+1
+        #swap the letters in the firstword with the next char
+        while (j > i):
+            chartoswap = firstword[j]
+            firstword[j] = firstword[j-1]
+            firstword[j-1] = chartoswap
+            j = j-1
+            #now that they are swapped, exit this while loop and check if the the newly swapped char, decrementing the current index j
 
-print(compareWords("ABCDEF", "ABCDEF"))
-print(compareWords("ABDCEF", "BBCDEF"))
-print(compareWords("BBDCEF", "BBCDEF"))
-print(compareWords("HARRIS", "HARIRS"))
-print(compareWords("BACDFE", "ABCDEF"))
-print(compareWords("ABCFDE", "ABCDEF"))
-print(compareWords("ANNE", "ENNA"))
-print(compareWords("FEDCBA", "ABCDEF"))
-print(compareWords("GFEDCBA", "ABCDEFG"))
+
+        i = i+1
+
+    return swapcount
+
+print("Actual score:  {} \nExpected score 100".format(compareWords("ABCDEF", "ABCDEF")))
+print("Actual score:  {} \nExpected score 0".format(compareWords("ABDCEF", "BBCDEF")))
+print("Actual score:  {} \nExpected score 95".format(compareWords("BBDCEF", "BBCDEF")))
+print("Actual score:  {} \nExpected score 95".format(compareWords("HARRIS", "HARIRS")))
+print("Actual score:  {} \nExpected score 95".format(compareWords("HARRIS", "HRARIS")))
+
+print("Actual score:  {} \nExpected score 90".format(compareWords("BACDFE", "ABCDEF")))
+print("Actual score:  {} \nExpected score 90".format(compareWords("ABCFDE", "ABCDEF")))
+
+print("Actual score:  {} \nExpected score 75".format(compareWords("ANNE", "ENNA")))
+print("Actual score:  {} \nExpected score 25".format(compareWords("FEDCBA", "ABCDEF")))
+print("Actual score:  {} \nExpected score 0".format(compareWords("GFEDCBA", "ABCDEFG")))
